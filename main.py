bbacb5bae921369304d0fc2583f9c524e4eece1f
shershen08 <michail.kuznetsov@gmail.com>
2025-12-19 13:01:33 +0000

    comment out __name__

--- main.py
+++ main.py
@@ -121,6 +121,6 @@ async def root():
     }
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
